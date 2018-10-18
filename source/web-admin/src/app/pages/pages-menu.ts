9abaf0ba28bf43cd899f4b0e3fa48b4b58aca099
hainv <hai213k572gmail.com>
2018-10-18 01:36:45 -0700

    Add code show menu by permission
    Signed-off-by: hainv <hai213k572gmail.com>

--- source/web-admin/src/app/pages/pages-menu.ts
+++ source/web-admin/src/app/pages/pages-menu.ts
@@ -5,224 +5,57 @@ export const MENU_ITEMS: NbMenuItem[] = [
     title: 'Quản lý tài khoản',
     icon: 'nb-person',
     link: '/pages/account',
+    data: ['Admin']
   },
   {
     title: 'Quản lý chức vụ',
     icon: 'nb-grid-a',
     link: '/pages/position',
+    data: ['Admin']
   },
   {
     title: 'DS sinh viên',
     icon: 'nb-list',
     link: '/pages/students',
+    data: ['Admin', 'Publicer']
   },
   {
     title: 'Quản lý lớp học',
     icon: 'nb-compose',
+    data: ['Admin', 'Publicer'],
     children: [
       {
         title: 'Danh sách lớp học',
         link: '/pages/classroom-list',
+        data: ['Admin', 'Publicer']
       },
       {
         title: 'Quản lý đào tạo',
         link: '/pages/study-manager',
+        data: ['Admin', 'Publicer']
       }
     ]
   },
   {
     title: 'Chứng chỉ',
     icon: 'nb-keypad',
+    data: ['Admin', 'Publicer'],
     children: [
       {
         title: 'Danh mục chứng chỉ',
         link: '/pages/cetificate-category',
+        data: ['Admin', 'Publicer']
       },
       {
         title: 'DS chứng chỉ',
         link: '/pages/cetificate-list',
+        data: ['Admin', 'Publicer']
       },
       {
         title: 'Lịch sử  cập nhật',
         link: '/pages/cetificate-log',
+        data: ['Admin', 'Publicer']
       }
     ]
-    // },
-    // {
-    //   title: 'E-commerce',
-    //   icon: 'nb-e-commerce',
-    //   link: '/pages/dashboard',
-    //   home: true,
-    // },
-    // {
-    //   title: 'IoT Dashboard',
-    //   icon: 'nb-home',
-    //   link: '/pages/iot-dashboard',
-    // },
-    // {
-    //   title: 'FEATURES',
-    //   group: true,
-    // },
-    // {
-    //   title: 'UI Features',
-    //   icon: 'nb-keypad',
-    //   link: '/pages/ui-features',
-    //   children: [
-    //     {
-    //       title: 'Buttons',
-    //       link: '/pages/ui-features/buttons',
-    //     },
-    //     {
-    //       title: 'Grid',
-    //       link: '/pages/ui-features/grid',
-    //     },
-    //     {
-    //       title: 'Icons',
-    //       link: '/pages/ui-features/icons',
-    //     },
-    //     {
-    //       title: 'Modals',
-    //       link: '/pages/ui-features/modals',
-    //     },
-    //     {
-    //       title: 'Popovers',
-    //       link: '/pages/ui-features/popovers',
-    //     },
-    //     {
-    //       title: 'Typography',
-    //       link: '/pages/ui-features/typography',
-    //     },
-    //     {
-    //       title: 'Animated Searches',
-    //       link: '/pages/ui-features/search-fields',
-    //     },
-    //     {
-    //       title: 'Tabs',
-    //       link: '/pages/ui-features/tabs',
-    //     },
-    //   ],
-    // },
-    // {
-    //   title: 'Forms',
-    //   icon: 'nb-compose',
-    //   children: [
-    //     {
-    //       title: 'Form Inputs',
-    //       link: '/pages/forms/inputs',
-    //     },
-    //     {
-    //       title: 'Form Layouts',
-    //       link: '/pages/forms/layouts',
-    //     },
-    //   ],
-    // },
-    // {
-    //   title: 'Components',
-    //   icon: 'nb-gear',
-    //   children: [
-    //     {
-    //       title: 'Tree',
-    //       link: '/pages/components/tree',
-    //     }, {
-    //       title: 'Notifications',
-    //       link: '/pages/components/notifications',
-    //     },
-    //   ],
-    // },
-    // {
-    //   title: 'Maps',
-    //   icon: 'nb-location',
-    //   children: [
-    //     {
-    //       title: 'Google Maps',
-    //       link: '/pages/maps/gmaps',
-    //     },
-    //     {
-    //       title: 'Leaflet Maps',
-    //       link: '/pages/maps/leaflet',
-    //     },
-    //     {
-    //       title: 'Bubble Maps',
-    //       link: '/pages/maps/bubble',
-    //     },
-    //     {
-    //       title: 'Search Maps',
-    //       link: '/pages/maps/searchmap',
-    //     },
-    //   ],
-    // },
-    // {
-    //   title: 'Charts',
-    //   icon: 'nb-bar-chart',
-    //   children: [
-    //     {
-    //       title: 'Echarts',
-    //       link: '/pages/charts/echarts',
-    //     },
-    //     {
-    //       title: 'Charts.js',
-    //       link: '/pages/charts/chartjs',
-    //     },
-    //     {
-    //       title: 'D3',
-    //       link: '/pages/charts/d3',
-    //     },
-    //   ],
-    // },
-    // {
-    //   title: 'Editors',
-    //   icon: 'nb-title',
-    //   children: [
-    //     {
-    //       title: 'TinyMCE',
-    //       link: '/pages/editors/tinymce',
-    //     },
-    //     {
-    //       title: 'CKEditor',
-    //       link: '/pages/editors/ckeditor',
-    //     },
-    //   ],
-    // },
-    // {
-    //   title: 'Tables',
-    //   icon: 'nb-tables',
-    //   children: [
-    //     {
-    //       title: 'Smart Table',
-    //       link: '/pages/tables/smart-table',
-    //     },
-    //   ],
-    // },
-    // {
-    //   title: 'Miscellaneous',
-    //   icon: 'nb-shuffle',
-    //   children: [
-    //     {
-    //       title: '404',
-    //       link: '/pages/miscellaneous/404',
-    //     },
-    //   ],
-    // },
-    // {
-    //   title: 'Auth',
-    //   icon: 'nb-locked',
-    //   children: [
-    //     {
-    //       title: 'Login',
-    //       link: '/auth/login',
-    //     },
-    //     {
-    //       title: 'Register',
-    //       link: '/auth/register',
-    //     },
-    //     {
-    //       title: 'Request Password',
-    //       link: '/auth/request-password',
-    //     },
-    //     {
-    //       title: 'Reset Password',
-    //       link: '/auth/reset-password',
-    //     },
-    //   ],
   }
 ];
--- source/web-admin/src/app/pages/pages.component.ts
+++ source/web-admin/src/app/pages/pages.component.ts
@@ -1,6 +1,8 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 import { MENU_ITEMS } from './pages-menu';
+
+import { Config } from '../config';
 
 @Component({
   selector: 'ngx-pages',
@@ -11,7 +13,43 @@ import { MENU_ITEMS } from './pages-menu';
     </ngx-sample-layout>
   `,
 })
-export class PagesComponent {
+export class PagesComponent implements OnInit {
 
   menu = MENU_ITEMS;
+
+
+  setHidenChildren(childrens: any[], role: string) {
+    if (childrens == undefined || role == undefined) {
+      return;
+    }
+    childrens.forEach((item) => {
+      item.data.forEach(tmp => {
+        if (tmp == role) {
+          item.hidden = false;
+          return;
+        } else {
+          item.hidden = true;
+        }
+      });
+    });
+  }
+
+  ngOnInit(): void {
+    let accountData = localStorage.getItem(Config.OJBJECT_KEY);
+    let account = JSON.parse(accountData);
+    let permission = account.position;
+
+    this.menu.forEach((item) => {
+      // Set defualt hidden menu
+      item.hidden = true;
+      if (item && item.data) {
+        item.data.forEach((role) => {
+          if (role == permission) {
+            item.hidden = false;
+            this.setHidenChildren(item.children, permission);
+          }
+        });
+      }
+    });
+  }
 }
