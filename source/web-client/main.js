2e9a741b56546fe0baed8312aa26db0f915595c2
hainv <hai213k572gmail.com>
2018-10-17 20:38:12 -0700

    Add express to web client
    Signed-off-by: hainv <hai213k572gmail.com>

---
+++ source/web-client/main.js
@@ -0,0 +1,14 @@
+var express = require("express");
+var app = express();
+var path = require("path");
+
+app.use(express.static(__dirname + '/public' ));
+var HTTP_PORT = 3001;
+
+app.get('/', function (req, res) {
+    res.sendFile(path.join(__dirname + '/index.html'));
+});
+
+app.listen(HTTP_PORT);
+
+console.log("Running at Port: ", HTTP_PORT);
