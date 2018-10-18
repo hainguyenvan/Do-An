d8e9689ac56dc1a2826129aac44f282afe06c263
hainv <hai213k57@gmail.com>
2018-10-18 09:43:09 +0700

    Edit code base of web client
    Signed-off-by: hainv <hai213k57@gmail.com>

---
+++ source/web-client/js/app.js
@@ -0,0 +1,18 @@
+(function($, document, window){
+	
+	$(document).ready(function(){
+
+		// Cloning main navigation for mobile menu
+		$(".mobile-navigation").append($(".main-navigation .menu").clone());
+
+		// Mobile menu toggle 
+		$(".menu-toggle").click(function(){
+			$(".mobile-navigation").slideToggle();
+		});
+	});
+
+	$(window).load(function(){
+
+	});
+
+})(jQuery, document, window);
---
+++ source/web-client/js/min/app-min.js
@@ -0,0 +1,1 @@
+!function($,n,o){$(n).ready(function(){}),$(o).ready(function(){})}(jQuery,document,window);
